9a5ee5aeb081b4540b3c27e751bc78ec388f3d48
stankovicuros27 <stankovicuros27@gmail.com>
2021-09-11 19:19:12 +0100

    Implemented election results

--- elections/admin/electionResultCalculator.py
+++ elections/admin/electionResultCalculator.py
@@ -10,7 +10,39 @@ def calculateIndividualElection(participantInfos, totalVotesOnElection):
         )
     return participantResults
 
-#TODO
 def calculatePartyElection(participantInfos, totalVotesOnElection):
+    threshold = 0.05
     seatsLeft = 250
-    return None
+    underThreshold = []
+    overThreshold = []
+    for participantInfo in participantInfos:
+        participantInfo["seatsSoFar"] = 0
+        if participantInfo["totalVotes"] / totalVotesOnElection >= threshold:
+            overThreshold.append(participantInfo)
+        else:
+            underThreshold.append(participantInfo)
+
+    while seatsLeft > 0:
+        leadingParty = None
+        maxQuof = 0
+        for party in overLine:
+            if party["totalVotes"] / (party["seatsSoFar"] + 1) > maxQuof:
+                maxQuof = party["totalVotes"] / (party["seatsSoFar"] + 1)
+                leadingParty = party
+
+        leadingParty["seatsSoFar"] += 1
+        seatsLeft -= 1
+
+    participantResults = []
+    for participantInfo in [*overThreshold, *underThreshold]:
+        participantResults.append(
+            {
+                "pollNumber": participantInfo["pollNumber"],
+                "name": participantInfo["name"],
+                "result": participantInfo["seatsSoFar"]
+            }
+        )
+    return participantResults
+
+
+
